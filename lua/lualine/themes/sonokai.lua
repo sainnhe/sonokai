06486a78c7d1bc509a12cbc30ab7312d811d9b94
Antoine Cotten <hello@acotten.com>
2025-08-02 19:19:08 +0200

    fix(lualine): use filled_*
    Fixes #124

--- lua/lualine/themes/sonokai.lua
+++ lua/lualine/themes/sonokai.lua
@@ -15,17 +15,17 @@ end
 
 return {
   normal = {
-    a = {bg = palette.bg_blue[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.filled_blue[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   insert = {
-    a = {bg = palette.bg_green[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.filled_green[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   visual = {
-    a = {bg = palette.bg_red[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.filled_red[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
